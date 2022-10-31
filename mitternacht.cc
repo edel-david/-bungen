2aba61cd5e1279b1d530d23a185e73208fcf045b
David Edel <david.edel@gmx.de>
2022-10-31 16:25:39 +0100

    formated

--- mitternacht.cc
+++ mitternacht.cc
@@ -1,18 +1,19 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
-int main(){
-    double a,b,c;
+int main()
+{
+    double a, b, c;
     std::cout << "a= " << std::flush;
-    std::cin >> a ;
+    std::cin >> a;
     std::cout << "b= " << std::flush;
-    std::cin >> b ;
+    std::cin >> b;
     std::cout << "c= " << std::flush;
-    std::cin >> c ;    
+    std::cin >> c;
 
-    double sqrt=std::sqrt((b*b) - (4*a*c));
-    double x1= (-b -sqrt) / (2*a);
-    double x2=(-b +sqrt) / (2*a);
+    double sqrt = std::sqrt((b * b) - (4 * a * c));
+    double x1 = (-b - sqrt) / (2 * a);
+    double x2 = (-b + sqrt) / (2 * a);
     std::cout << "Die Ergebnisse sind " << x1 << " und " << x2 << std::endl;
     return 0;
 }
