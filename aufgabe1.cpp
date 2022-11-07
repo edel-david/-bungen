cd0c4b1856fcb0f25dcebc9f3609f8e276600671
David Edel <david.edel@gmx.de>
2022-11-07 12:36:52 +0100

    aufgabe 1 finished

--- aufgabe1.cpp
+++ aufgabe1.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-int main()
-{
-}
 
 void collatz(int number)
 {
-    std::cout << number << std::endl;
+    
     while (number != 1 & number != 0 & number != -1 & number != -5 & number != -17)
+    
     {
+        std::cout << number << std::endl;
         if (number % 2 == 0)
         {
             number /= 2;
@@ -19,3 +18,12 @@ void collatz(int number)
     }
     std::cout << number << std::endl;
 }
+
+int main()
+{
+    long num;
+    std::cout << "gib eine Nummer ein: " << std::endl;
+    std::cin >> num ;
+    collatz(num);
+}
+
