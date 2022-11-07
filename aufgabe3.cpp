f8eec436f9f3aec95ef3eb243c4462dd58684380
David Edel <david.edel@gmx.de>
2022-11-07 16:53:43 +0100

    added aufgabe 3 (hoffentlich richtig)

---
+++ aufgabe3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+int iterative(int q, int n)
+{
+    int erg = 1;
+    for (int i = 0; i < n; i++)
+    {
+        erg *= q;
+    }
+    return erg;
+}
+
+int recursive(int q, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return recursive(q, n - 1) * q;
+    }
+}
+
+int better(int q, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    if (n % 2 == 0)
+    {
+        int faktor = better(q, n / 2);
+        return faktor * faktor;
+    }
+    else
+    {
+        return better(q, n - 1) * q;
+    }
+}
+
+int main(int argc, const char **argv)
+{
+    int base = 5;
+    int pow = 9;
+    std::cout << iterative(base, pow) << " and " << recursive(base, pow) << " and " << better(base, pow) << std::endl;
+    return 0;
+}
